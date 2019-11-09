d7b3e0ba4e6e0f27bea3930ea029f99e24f6587d
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2019-11-08 19:35:47 -0500

    completed 1st step of constructor

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,7 +5,12 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-
+const CuboidMaker= function(length, width, property){
+  this.length= length;
+  this.width= width;
+  this.property= property;
+}
+// console.log(CuboidMaker);
 
 /* == Step 2: Volume Method ==
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
